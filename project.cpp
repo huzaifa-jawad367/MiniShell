df3c2682fb1e5f5eaecbfac2c3f1d634f6d63d39
Huzaifa Jawad <hjawad.bscs21seecs@seecs.edu.pk>
2023-05-21 15:59:54 +0500

    random commit

--- project.cpp
+++ project.cpp
@@ -157,6 +157,7 @@ void myExecvp(char **argv) {
             // Execute a single command if no pipe is present
             execvp(argv[0], argv);
             perror("execvp");
+			
             exit(1);
         }
     } else if (pid < 0) {
@@ -167,40 +168,7 @@ void myExecvp(char **argv) {
     }
 
 }
-// void  myExecvp(char **argv)
-// {
-// 	pid_t  pid;
-// 	int    status;
-// 	int childStatus;
-// 	pid = fork();
-// 	if(pid == 0)
-// 	{
-// 		if (strcmp(argv[0], "history") == 0)
-// 		{
-// 			PrintHistory();
-// 			return;
-// 		}
-		
-// 		childStatus = execvp(*argv, argv);
-// 		if (childStatus < 0){
-// 			cout<<"ERROR:wrong input"<<endl;
-// 		}
-// 		exit(0);
 
-// 	}
-
-// 	else if(pid < 0)
-// 	{
-// 		cout<< "somthing went wrong!"<<endl;
-// 	}
-// 	else 
-
-// 	{   int status;
-// 		waitpid(pid, &status , 0);
-
-// 	}
-
-// }
 
 void StrTokenizer(char *input, char **argv) {
     char *stringTokenized;
