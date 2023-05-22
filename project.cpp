5a91d10120a4402c9efc0918214e0dd792260b72
Huzaifa Jawad <hjawad.bscs21seecs@seecs.edu.pk>
2023-05-22 15:13:44 +0500

    Added chmod and chown command

--- project.cpp
+++ project.cpp
@@ -8,6 +8,9 @@
 #include <cctype>
 #include <algorithm>
 #include <filesystem>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <cstdlib> 
 
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -25,6 +28,8 @@ void Grep(string filePath, string word);
 bool isWordInSentence(const string& sentence, const string& word);
 void renameFile(const string& filePath, const string& newName);
 void createFile(const string& filePath);
+bool changeFileOwner(const std::string& filePath, const uid_t& userId, const gid_t& groupId);
+bool changeFilePermissions(const string& filePath, const mode_t& permissions);
 
 vector<string> history;
 
@@ -77,18 +82,43 @@ void myExecvp(char **argv) {
 			PrintHistory();
 			return;
 		}
-
-        if (strcmp(argv[0], "mv") == 0)
+        // call mv to rename or move file (havent tested moving)
+        else if (strcmp(argv[0], "mv") == 0)
 		{
 			renameFile(string(argv[1]), string(argv[2]));
 			return;
 		}
-
-        if (strcmp(argv[0], "touch") == 0)
+        // call touch to create file
+        else if (strcmp(argv[0], "touch") == 0)
 		{
 			createFile(argv[1]);
 			return;
 		}
+
+        // call chown to change owners
+        else if (strcmp(argv[0], "chown") == 0)
+		{
+            uid_t userId = std::stoi(argv[2]);
+            gid_t groupId = std::stoi(argv[3]);
+
+            // Use the converted uid_t and gid_t values as needed
+            std::cout << "User ID: " << userId << std::endl;
+            std::cout << "Group ID: " << groupId << std::endl;
+            
+            
+			changeFileOwner(argv[1], userId, groupId);
+			return;
+		}
+
+        // call chmod to change permission
+        else if (strcmp(argv[0], "chmod") == 0)
+		{
+            // mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+            mode_t permissions = strtol(argv[2], nullptr, 8);
+			changeFilePermissions(argv[1], permissions);
+			return;
+		}
+
 
         // Check for '>>' operator
         for (int i = 0; argv[i] != NULL; i++) {
@@ -228,6 +258,7 @@ void GetEnv()
 	arr2[k] = NULL;
 }
 
+// history command
 void PrintHistory() {
     int start = (history.size() > 10) ? (history.size() - 10) : 0;
     for (size_t i = start; i < history.size(); i++) {
@@ -250,6 +281,7 @@ bool isWordInSentence(const string& sentence, const string& word) {
     return (found != string::npos);
 }
 
+// GREP command
 void Grep(string filePath, string word) {
 	ifstream inputFile(filePath);
 	if (inputFile.is_open()) {
@@ -266,6 +298,7 @@ void Grep(string filePath, string word) {
 	inputFile.close();
 }
 
+// rename file "mv" command
 void renameFile(const string& filePath, const string& newName) {
     filesystem::path oldPath(filePath);
     filesystem::path newPath = oldPath.parent_path() / newName;
@@ -278,6 +311,7 @@ void renameFile(const string& filePath, const string& newName) {
     }
 }
 
+// create file command
 void createFile(const string& filePath) {
     ofstream file(filePath);  // Open file in output mode
 
@@ -286,5 +320,26 @@ void createFile(const string& filePath) {
         file.close();  // Close the file
     } else {
         cerr << "Failed to create the file." << endl;
+    }
+}
+
+// chown command
+bool changeFileOwner(const string& filePath, const uid_t& userId, const gid_t& groupId) {
+    if (chown(filePath.c_str(), userId, groupId) == 0) {
+        cout << "File owner changed successfully." << endl;
+        return true;
+    } else {
+        cerr << "Failed to change file owner." << endl;
+        return false;
+    }
+}
+
+bool changeFilePermissions(const string& filePath, const mode_t& permissions) {
+    if (chmod(filePath.c_str(), permissions) == 0) {
+        cout << "File permissions changed successfully." << endl;
+        return true;
+    } else {
+        cerr << "Failed to change file permissions." << endl;
+        return false;
     }
 }
